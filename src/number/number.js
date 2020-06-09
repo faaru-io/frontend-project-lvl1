e0398103362573101e8c98cd9d08ea3790c66e5b
aschweizer <anton.schweizer@loeffelhardt.de>
2020-06-09 06:38:09 +0200

    fix random number generator: out of range value

--- src/number/number.js
+++ src/number/number.js
@@ -1,5 +1,5 @@
 const generateNumberBetween = (min, max) => (
-  Math.floor(Math.random() * (max - min + 1)) + min
+  Math.floor(Math.random() * (max - min)) + min
 );
 
 export default generateNumberBetween;
